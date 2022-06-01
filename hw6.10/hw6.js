ea5e019c726ba789bf289c500842773f4042d064
ayazimahsa <mahsaayazii73@gmail.com>
2022-06-02 02:17:43 +0430

    add hw6

---
+++ hw6.10/hw6.js
@@ -0,0 +1,10 @@
+// What do you think? Will the .catch trigger? Explain your answer.
+
+new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        throw new Error("Whoops!");
+    }, 1000);
+}).catch(alert);
+
+// .catch(f) is the same as promise.then(null, f)
+// shows "Error: Whoops!" after 1 second.
